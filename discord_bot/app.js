cf2c686e498e6c5fefcdd5f17798250530cae3e1
Dimitri Vervecken <dimitri.vervecken@gmail.com>
2018-11-18 21:50:10 +0100

    Added some improvements; updated matchmaker(case&diacritics insensetive); spelling mistakes

--- discord_bot/app.js
+++ discord_bot/app.js
@@ -3,18 +3,23 @@ var prefix = "~uname_test"
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const settings = require('./settings.json');
+const removeDiacritics = require('diacritics').remove;
 
 client.on('ready',() => {
 	console.log('Bot started');
 });
+//console.log(removeDiacritics("ZZZ(Sahzz)".toLowerCase()).includes(removeDiacritics("Sahzz".toLowerCase())))
+//console.log("ZDD(Sahzz)".includes("Sahzz"))
 
 client.on('message', message => {
 	if (message.author === client.user) return;
 	if (message.content.startsWith(prefix)) {
+		var channel_found = false;
 		console.log('Got a message for me: '+message.content);
 		var cmnd_array = message.content.split(" ");
 		message.guild.channels.forEach(function(channel){
 			if ( channel.name === cmnd_array[1] && channel.type === "voice" ){
+				channel_found = true;
 				var yes = [];
                                 var no = [];
 				var no2 = [];
@@ -28,8 +33,9 @@ client.on('message', message => {
 						name = member.nickname;
 					}
 					cmnd_array.slice(2).forEach(function(name_g){
-						if (name_g === name){
+						if (removeDiacritics(name.toLowerCase()).includes(removeDiacritics(name_g.toLowerCase()))){
 							found = true;
+							name = name_g;
 						}
 					})
 					if(found){
@@ -42,7 +48,7 @@ client.on('message', message => {
 					found = false;
 					yes.forEach(function(name_on){
 						if (name_g === name_on){
-                                                        found = true;
+                                                        found = true
                                                 }
 					})
 					if(found){
@@ -51,11 +57,14 @@ client.on('message', message => {
 						no2.push(name_g);
 					}
 				})
-				message.channel.sendMessage('Playes found ('+yes.length+'): '+yes);
-				message.channel.sendMessage('Unknown Discord nicknames('+no.length+'): '+no);
-				message.channel.sendMessage('Unknown Ingame players:('+no2.length+')'+no2);
+				message.channel.send('Players matched ('+yes.length+'): '+yes.sort());
+				message.channel.send('Unknown Discord nicknames ('+no.length+'): '+no.sort());
+				message.channel.send('Unknown Ingame players ('+no2.length+'): '+no2.sort());
 			}
 		})
+		if(!channel_found){
+			message.channel.send("Channel "+cmnd_array[1]+" not found!");
+		}
 	}
 });
 
